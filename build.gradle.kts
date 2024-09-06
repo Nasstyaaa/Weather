d67d5c593047128df51fd5edc4e82560e04befcd
Nasstyaaa <Luk-2004@mail.ru>
2024-09-06 17:00:09 +0300

    feat: the basic structure of servlets is defined

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,9 @@ java {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.1.0")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
 
 tasks.withType<War> {
---
+++ src/main/java/org/nastya/servlet/UserServlet.java
@@ -0,0 +1,18 @@
+package org.nastya.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/user")
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+    }
+}
