9d7af3b1449bb911a1343af52b20f4a7dd1e9617
Nasstyaaa <Luk-2004@mail.ru>
2024-09-06 20:35:12 +0300

    feat: configuration and testing Thymeleaf

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
     implementation("org.hibernate.orm:hibernate-core:6.6.0.Final")
+    implementation("org.thymeleaf:thymeleaf:3.1.2.RELEASE")
 }
 
 tasks.withType<War> {
--- src/main/java/org/nastya/servlet/UserServlet.java
+++ src/main/java/org/nastya/servlet/UserServlet.java
@@ -5,14 +5,22 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.nastya.config.ThymeleafConfig;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/user")
+@WebServlet(urlPatterns = "/")
 public class UserServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        TemplateEngine engine = ThymeleafConfig.buildTemplateEngine(req.getServletContext());
+
+        WebContext context = ThymeleafConfig.buildWebContext(req, resp, getServletContext());
+        context.setVariable("recipient", "World");
+        engine.process("index", context, resp.getWriter());
     }
 }
