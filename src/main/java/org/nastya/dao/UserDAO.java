7d2df0d24120f6c506dd8d067ae44d142a031b8c
Nasstyaaa <Luk-2004@mail.ru>
2024-09-07 15:10:27 +0300

    feat: create DataListenerUtil

---
+++ src/main/java/org/nastya/dao/UserDAO.java
@@ -0,0 +1,11 @@
+package org.nastya.dao;
+
+import org.nastya.model.User;
+
+import java.lang.module.Configuration;
+
+public class UserDAO {
+
+    public void save(User user){
+    }
+}
---
+++ src/main/java/org/nastya/util/DataListenerUtil.java
@@ -0,0 +1,27 @@
+package org.nastya.util;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.nastya.model.Location;
+import org.nastya.model.Session;
+import org.nastya.model.User;
+
+
+public class DataListenerUtil implements ServletContextListener{
+    public static SessionFactory sessionFactory;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Session.class)
+                .addAnnotatedClass(Location.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public synchronized static org.hibernate.Session getSession(){
+        return sessionFactory.openSession();
+    }
+}
